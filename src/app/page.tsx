5623076c39d494f96eb33765ddf6d2c6d8270a67
Jani Eväkallio <jani.evakallio@gmail.com>
2023-07-21 16:52:26 +0100

    Tweak visual experience when hosted on partykit.io

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
-import Link from "next/link"
-import SharedSpace from "./shared-space"
-import CursorsContextProvider from "./cursors-context"
+import Link from "next/link";
+import SharedSpace from "./shared-space";
+import CursorsContextProvider from "./cursors-context";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  // when hosted in an iframe on the partykit website, don't render link to the site
+  const isPartyKitWebsite = searchParams?.host === "io";
+
   return (
     <main className="flex flex-col gap-4 min-h-screen p-6 overflow-hidden select-none">
-      <div className="absolute top-3 right-3 text-sm">
-        Made with <Link className="underline" href="https://partykit.io">PartyKit</Link>
-      </div>
+      {isPartyKitWebsite ? null : (
+        <div className="absolute top-3 right-3 text-sm">
+          Made with{" "}
+          <Link className="underline" href="https://partykit.io">
+            PartyKit
+          </Link>
+        </div>
+      )}
 
       <CursorsContextProvider>
         <SharedSpace />
       </CursorsContextProvider>
     </main>
-  )
+  );
 }
